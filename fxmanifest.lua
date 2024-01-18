968ece0e32425b3d0b547fd5694f82990bb7ac06
Alzon22 <gsxisto@gmail.com>
2024-01-17 23:07:58 -0300

    FXManifest changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version 'cerulean' 
 game 'gta5'
 
+name 'OneSource HUD'
+author 'Xisto & Marquez'
+version '1.0.0'
+discord 'https://discord.com/invite/kNGX6NvrUP'
+
 ui_page 'client/nui/index.html'
 
 client_script "client/main.lua"
